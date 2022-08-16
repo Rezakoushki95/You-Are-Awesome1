453db3b0c09be4d6accaacab10433d64f1a08739
rezakoushki95 <rezaaarhus@hotmail.com>
2022-08-16 20:01:17 +0200

    Section 2.13.Getting Loopy with While and Repeat

--- You Are Awesome/ViewController.swift
+++ You Are Awesome/ViewController.swift
@@ -12,8 +12,10 @@ class ViewController: UIViewController {
 	@IBOutlet weak var messageLabel: UILabel!
 	@IBOutlet weak var imageView: UIImageView!
 	
-	var imageNumber = 0
-		
+	var messageNumber = -1
+	var imageNumber = -1
+	let totalNumberOfImages = 9
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		messageLabel.text = ""
@@ -22,28 +24,33 @@ class ViewController: UIViewController {
 	
 	@IBAction func messageButtonPressed(_ sender: UIButton) {
 		
-		print(imageNumber)
-//		let imageName = "image" + String(imageNumber)
-		let imageName = "image\(imageNumber)" 
-		imageView.image = UIImage(named: imageName)
-		imageNumber += 1
-		if imageNumber == 10 {
-			imageNumber = 0
-		}
-//		let awesomeMessage = "You Are Awesome!"
-//		let greatMessage = "You Are Great!"
-//		let bombMessage = "You Are Da Bomb!"
-//
-//		if messageLabel.text == awesomeMessage {
-//			messageLabel.text = greatMessage
-//			imageView.image = UIImage(named: "image1")
-//		} else if messageLabel.text == greatMessage {
-//			messageLabel.text = bombMessage
-//			imageView.image = UIImage(named: "image2")
-//		} else {
-//			messageLabel.text = awesomeMessage
-//			imageView.image = UIImage(named: "image0")
-//		}
+		let messages = [
+			"You Are Awesome!",
+			"You Are Great!",
+			"You Are Da Bomb!",
+			"You Suck!",
+			"You Fine!"
+		]
+		
+		var newMessageNumber: Int
+		
+
+		repeat {
+			newMessageNumber = Int.random(in: 0...messages.count-1)
+
+		} while messageNumber == newMessageNumber
+		messageNumber = newMessageNumber
+		messageLabel.text = messages[messageNumber]
+		
+		var newImageNumber: Int
+		repeat {
+			newImageNumber = Int.random(in: 0...totalNumberOfImages)
+		} while imageNumber == newImageNumber
+		
+		imageNumber = newMessageNumber
+		imageView.image = UIImage(named: "image\(imageNumber)")
+
 	}
-}
+  }
+ 
  
