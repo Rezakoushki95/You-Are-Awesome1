4ce734b7034e1e70d73da8f5ac6e3f019fc8ac3b
rezakoushki95 <rezaaarhus@hotmail.com>
2022-08-17 14:52:28 +0200

    Section 2.15.Playing Sounds with AVAudioPlayer, and Error Handling with If Let and Do Catch

--- You Are Awesome/ViewController.swift
+++ You Are Awesome/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 	
@@ -15,6 +16,8 @@ class ViewController: UIViewController {
 	var messageNumber = -1
 	var imageNumber = -1
 	let totalNumberOfImages = 9
+	
+	var audioPlayer: AVAudioPlayer!
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -49,6 +52,17 @@ class ViewController: UIViewController {
 		
 		imageNumber = newMessageNumber
 		imageView.image = UIImage(named: "image\(imageNumber)")
+		
+		if let sound = NSDataAsset(name: "sound\(Int.random(in: 0...5))") {
+			do {
+				try audioPlayer = AVAudioPlayer(data: sound.data)
+				audioPlayer.play()
+			} catch {
+				print("🤬 ERROR: \(error.localizedDescription)- Could not initialize AVAudioPlayer object")
+			}
+		} else {
+			print("🤬 ERROR - Could not read data from file sound0")
+		}
 
 	}
   }
